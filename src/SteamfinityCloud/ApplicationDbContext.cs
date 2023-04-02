c1e9bb2c3f5d6d6f4a4e4d7690a6c657fb16e11a
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-02 18:20:14 +0200

    Add ApplicationDbContext Class

---
+++ src/SteamfinityCloud/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Steamfinity.Cloud.Entities;
+
+namespace Steamfinity.Cloud;
+
+/// <summary>
+/// Represents a database context in the Steamfinity Cloud.
+/// </summary>
+public sealed class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApplicationDbContext"/> class.
+    /// </summary>
+    /// <param name="options">The options to be used by the database context.</param>
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+}
