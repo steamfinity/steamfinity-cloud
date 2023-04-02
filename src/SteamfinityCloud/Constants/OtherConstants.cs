5073312c007f0766b3c41130347eb69fb97ceb29
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-02 22:41:56 +0200

    Add OtherConstants Class

---
+++ src/SteamfinityCloud/Constants/OtherConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Steamfinity.Cloud.Constants;
+
+/// <summary>
+/// Provides a collection of constants used throughout the application.
+/// </summary>
+public static class OtherConstants
+{
+    public const int DefaultMaximumLenght = 256;
+}
