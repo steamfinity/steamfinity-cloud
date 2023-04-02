e2ea987cada9a15f1519e0ab962da826b65064eb
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-02 18:59:55 +0200

    Register RoleInitializer Service

--- src/SteamfinityCloud/Program.cs
+++ src/SteamfinityCloud/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Steamfinity.Cloud;
 using Steamfinity.Cloud.Entities;
+using Steamfinity.Cloud.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.AddScoped<IRoleInitializer, RoleInitializer>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
