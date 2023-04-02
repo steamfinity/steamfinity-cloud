313d1fd8eafd7f80254d71609f9fb156920eae13
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-02 18:58:49 +0200

    Add IdentityException Class

---
+++ src/SteamfinityCloud/Exceptions/IdentityException.cs
@@ -0,0 +1,13 @@
+﻿namespace Steamfinity.Cloud.Exceptions;
+
+/// <summary>
+/// Represents an exception that is thrown when an identity error occurs in the Steamfinity Cloud.
+/// </summary>
+public sealed class IdentityException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IdentityException"/> class with a specified error code.
+    /// </summary>
+    /// <param name="errorCode">The code that represents the identity error.</param>
+    public IdentityException(string errorCode) : base($"An identity error has occurred: '{errorCode}'.") { }
+}
