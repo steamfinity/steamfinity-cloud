6d7647ffda5ecd3a26a8d0cfea081b9380321811
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-02 18:59:06 +0200

    Add RoleNames Class

---
+++ src/SteamfinityCloud/Constants/RoleNames.cs
@@ -0,0 +1,10 @@
+﻿namespace Steamfinity.Cloud.Constants;
+
+/// <summary>
+/// Specifies the role names in the Steamfinity Cloud authorization system.
+/// </summary>
+public static class RoleNames
+{
+    public const string User = "User";
+    public const string Administrator = "Administrator";
+}
