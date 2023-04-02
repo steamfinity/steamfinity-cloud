f2ca7aa6487ec572124a71479eccd9d880f557cb
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-02 18:59:17 +0200

    Add IRoleInitializer Interface

---
+++ src/SteamfinityCloud/Services/IRoleInitializer.cs
@@ -0,0 +1,14 @@
+﻿namespace Steamfinity.Cloud.Services;
+
+/// <summary>
+/// Initializes the roles in the Steamfinity Cloud authorization system.
+/// </summary>
+public interface IRoleInitializer
+{
+    /// <summary>
+    /// Initializes the roles in the Steamfinity Cloud authorization system. Roles that already exist will be skipped.
+    /// Make sure to call this method on application startup.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task InitializeRolesAsync();
+}
