d95c91e417da3e3e977cbb25e8a97970beff46ec
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-02 18:17:08 +0200

    Add ApplicationRole Class

---
+++ src/SteamfinityCloud/Entities/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Steamfinity.Cloud.Entities;
+
+/// <summary>
+/// Represents a user role in the Steamfinity Cloud authorization system.
+/// </summary>
+public sealed class ApplicationRole : IdentityRole<Guid> { }
