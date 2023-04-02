e360d95e0147543c8e5671252ac61c0ee2eacdf4
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-02 18:13:26 +0200

    Add ApplicationUser Class

---
+++ src/SteamfinityCloud/Entities/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Steamfinity.Cloud.Entities;
+
+/// <summary>
+/// Represents a user of the Steamfinity Cloud.
+/// </summary>
+public sealed class ApplicationUser : IdentityUser<Guid>
+{
+    /// <summary>
+    /// Gets or sets a value indicating whether the user is currently suspended. 
+    /// A suspended user is prevented from authenticating and using the application.
+    /// </summary>
+    public bool IsSuspended { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date and time when the user created their Steamfinity Cloud account.
+    /// </summary>
+    public DateTimeOffset SignUpTime { get; init; } = DateTimeOffset.UtcNow;
+
+    /// <summary>
+    /// Gets or sets the date and time when the user last signed in to the Steamfinity Cloud.
+    /// This value is updated with every successful authentication token renewal.
+    /// </summary>
+    public DateTimeOffset? LastSignInTime { get; set; }
+}
