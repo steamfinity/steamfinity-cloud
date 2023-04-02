5cf0439fc78cc7d1a3c3531e9bbc13e1553d4c84
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-02 22:44:02 +0200

    Add TokenRefreshRequest Record

---
+++ src/SteamfinityCloud/Models/TokenRefreshRequest.cs
@@ -0,0 +1,24 @@
+﻿using Steamfinity.Cloud.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Steamfinity.Cloud.Models;
+
+/// <summary>
+/// Represents a user's request to refresh their temporary JWT bearer authentication token.
+/// </summary>
+
+public sealed record TokenRefreshRequest
+{
+    /// <summary>
+    /// Gets or sets the user ID associated with the refresh token.
+    /// </summary>
+    [Required]
+    public required Guid UserId { get; init; }
+
+    /// <summary>
+    /// Gets or sets the refresh token used to refresh the authentication token.
+    /// </summary>
+    [Required]
+    [MaxLength(OtherConstants.DefaultMaximumLenght)]
+    public required string RefreshToken { get; init; }
+}
