2e786b5aed511d0403dbee2fe50ea2f59b5a1d96
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-05 00:37:05 +0200

    Add UserNameChangeRequest Record

---
+++ src/SteamfinityCloud/Models/UserNameChangeRequest.cs
@@ -0,0 +1,18 @@
+﻿using Steamfinity.Cloud.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Steamfinity.Cloud.Models;
+
+/// <summary>
+/// Represents a user's request to change their username.
+/// </summary>
+public sealed record UserNameChangeRequest
+{
+    /// <summary>
+    /// Gets or sets the new username that the user will have when the request is accepted.
+    /// </summary>
+    /// <value>The new username.</value>
+    [Required]
+    [MaxLength(OtherConstants.DefaultMaximumLenght)]
+    public required string NewUserName { get; init; }
+}
