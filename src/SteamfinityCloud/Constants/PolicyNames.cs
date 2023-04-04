033258aded7faebf7d5d2d6ef50d1de870eff403
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-04 19:36:39 +0200

    Add PolicyNames Class

---
+++ src/SteamfinityCloud/Constants/PolicyNames.cs
@@ -0,0 +1,7 @@
+﻿namespace Steamfinity.Cloud.Constants;
+
+public static class PolicyNames
+{
+    public const string Users = "Users";
+    public const string Administrators = "Administrators";
+}
