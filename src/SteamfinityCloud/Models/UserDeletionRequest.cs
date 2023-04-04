f632295135121eccaee605fa9c79756ae8adb6ee
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-05 00:37:56 +0200

    Add UserDeletionRequest Record

---
+++ src/SteamfinityCloud/Models/UserDeletionRequest.cs
@@ -0,0 +1,18 @@
+﻿using Steamfinity.Cloud.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Steamfinity.Cloud.Models;
+
+/// <summary>
+/// Represents a user's request to delete their account.
+/// </summary>
+public sealed record UserDeletionRequest
+{
+    /// <summary>
+    /// Gets or sets the user's password. The correct password must be provided. Otherwise, the request won't be accepted.
+    /// </summary>
+    /// <value> The user's password.</value>
+    [Required]
+    [MaxLength(OtherConstants.DefaultMaximumLenght)]
+    public required string Password { get; init; }
+}
