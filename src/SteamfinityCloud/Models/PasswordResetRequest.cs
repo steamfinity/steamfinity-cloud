cf95bd05cf2c4cf69c60c566905daaf481be1e74
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-05 00:37:48 +0200

    Add PasswordResetRequest Record

---
+++ src/SteamfinityCloud/Models/PasswordResetRequest.cs
@@ -0,0 +1,18 @@
+﻿using Steamfinity.Cloud.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Steamfinity.Cloud.Models;
+
+/// <summary>
+/// Represents an administrator's request to reset a user's password.
+/// </summary>
+public sealed record PasswordResetRequest
+{
+    /// <summary>
+    /// Gets or sets the new password that the user will have when the request is accepted.
+    /// </summary>
+    /// <value>The new user's password.</value>
+    [Required]
+    [MaxLength(OtherConstants.DefaultMaximumLenght)]
+    public required string NewPassword { get; init; }
+}
