6f30183474fb4257eafaf1940ff3fc3a18b6605c
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-05 00:37:39 +0200

    Add PasswordChangeRequest Record

---
+++ src/SteamfinityCloud/Models/PasswordChangeRequest.cs
@@ -0,0 +1,26 @@
+﻿using Steamfinity.Cloud.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Steamfinity.Cloud.Models;
+
+/// <summary>
+/// Represents a user's request to change their password.
+/// </summary>
+public sealed record PasswordChangeRequest
+{
+    /// <summary>
+    /// Gets or sets the current user's password. The correct password must be provided. Otherwise, the request won't be accepted.
+    /// </summary>
+    /// <value>The current user's password.</value>
+    [Required]
+    [MaxLength(OtherConstants.DefaultMaximumLenght)]
+    public required string CurrentPassword { get; init; }
+
+    /// <summary>
+    /// Gets or sets the new password that the user will have when the request is accepted.
+    /// </summary>
+    /// <value>The new user's password.</value>
+    [Required]
+    [MaxLength(OtherConstants.DefaultMaximumLenght)]
+    public required string NewPassword { get; init; }
+}
