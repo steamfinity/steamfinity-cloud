d5bb0eb37d0a4e2d93fbf754cdb93540da5c2980
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-04 19:35:27 +0200

    Register JWT Bearer Authentication Service

--- src/SteamfinityCloud/Program.cs
+++ src/SteamfinityCloud/Program.cs
@@ -1,14 +1,35 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Steamfinity.Cloud;
 using Steamfinity.Cloud.Entities;
+using Steamfinity.Cloud.Exceptions;
 using Steamfinity.Cloud.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddScoped<IRoleInitializer, RoleInitializer>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters.ValidIssuer = builder.Configuration["Authentication:Schemes:Issuer"];
+    options.TokenValidationParameters.ValidAudience = builder.Configuration["Authentication:Schemes:Audience"];
+
+    var issuerSigningKey = builder.Configuration["Authentication:Schemes:IssuerSigningKey"]
+    ?? throw new ConfigurationMissingException("Authentication:Schemes:IssuerSigningKey");
+
+    options.TokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(issuerSigningKey));
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
