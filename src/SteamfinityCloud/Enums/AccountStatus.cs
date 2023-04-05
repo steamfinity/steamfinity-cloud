aa829dcf6f8cd18a45f431025ed7208292ed9886
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-05 16:14:34 +0200

    Add AccountStatus Enum

---
+++ src/SteamfinityCloud/Enums/AccountStatus.cs
@@ -0,0 +1,17 @@
+﻿using Steamfinity.Cloud.Entities;
+
+namespace Steamfinity.Cloud.Enums;
+
+/// <summary>
+/// Represents a status of a <see cref="SteamAccount"/>.
+/// </summary>
+public enum AccountStatus
+{
+    Offline,
+    Online,
+    Busy,
+    Away,
+    Snooze,
+    LookingToTrade,
+    LookingToPlay
+}
