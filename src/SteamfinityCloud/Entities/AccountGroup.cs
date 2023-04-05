f56ef3d9035df49895c221cc0925512699ce5e85
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-05 16:27:11 +0200

    Add AccountGroup Class

---
+++ src/SteamfinityCloud/Entities/AccountGroup.cs
@@ -0,0 +1,45 @@
+﻿namespace Steamfinity.Cloud.Entities;
+
+/// <summary>
+/// Represents a group of Steam accounts. This is an internal database entity that should not be exposed to the client.
+/// </summary>
+public sealed class AccountGroup
+{
+    /// <summary>
+    /// Gets or sets the unique identifier of the group.
+    /// </summary>
+    public required Guid Id { get; init; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the user that is the owner of the group.
+    /// </summary>
+    public required Guid OwnerId { get; init; }
+
+    /// <summary>
+    /// Gets the user that is the owner of the group.
+    /// </summary>
+    public ApplicationUser Owner { get; } = null!;
+
+    /// <summary>
+    /// Gets or sets the name of the group.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the description of the group.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Steam launch parameters specified for all accounts in the group.
+    /// </summary>
+    /// <remarks>
+    /// Group launch parameters have priority over global launch parameters but are overridden by per-account launch parameters.
+    /// </remarks>
+    public string? LaunchParameters { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date and time when the group was created.
+    /// </summary>
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+}
