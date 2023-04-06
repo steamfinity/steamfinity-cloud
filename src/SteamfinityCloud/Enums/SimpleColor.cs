a3a2ada2b4f9881ccccbd4559c8e5e6495f623f0
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-06 13:35:05 +0200

    Add SimpleColor Enum

---
+++ src/SteamfinityCloud/Enums/SimpleColor.cs
@@ -0,0 +1,18 @@
+﻿using Steamfinity.Cloud.Entities;
+
+namespace Steamfinity.Cloud.Enums;
+
+/// <summary>
+/// Represents a color associated with <see cref="SteamAccount"/> or <see cref="AccountGroup"/>.
+/// </summary>
+public enum SimpleColor
+{
+    None,
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Blue,
+    Purple,
+    Gray
+}
