ff7c253f0df0fb44002f4e29011bd841e59f132e
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-07 16:35:09 +0200

    Add MemberRole Enum

---
+++ src/SteamfinityCloud/Enums/MemberRole.cs
@@ -0,0 +1,16 @@
+﻿using Steamfinity.Cloud.Entities;
+
+namespace Steamfinity.Cloud.Enums;
+
+/// <summary>
+/// Represents a role of a shared library member.
+/// </summary>
+/// <remarks>
+/// Do not confuse with <see cref="ApplicationRole"/>.
+/// </remarks>
+public enum MemberRole
+{
+    Guest,
+    Member,
+    Administrator
+}
