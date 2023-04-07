551edc5e3d353b5a48af9b247ebb33ed50c176dd
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-07 16:35:21 +0200

    Add Membership Class

---
+++ src/SteamfinityCloud/Entities/Membership.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Steamfinity.Cloud.Enums;
+
+namespace Steamfinity.Cloud.Entities;
+
+/// <summary>
+/// Represents a membership of a user in a shared account library.
+/// </summary>
+[PrimaryKey(nameof(LibraryId), nameof(UserId))]
+public sealed class Membership
+{
+    /// <summary>
+    /// Gets or sets the identifier of the library that the user is a member of.
+    /// </summary>
+    public required Guid LibraryId { get; init; }
+
+    /// <summary>
+    /// Gets the library that the user is a member of.
+    /// </summary>
+    public Library Library { get; } = null!;
+
+    /// <summary>
+    /// Gets or sets the identifier of the user that is a member of the library.
+    /// </summary>
+    public required Guid UserId { get; init; }
+
+    /// <summary>
+    /// Gets the user that is a member of the library.
+    /// </summary>
+    public ApplicationUser User { get; } = null!;
+
+    /// <summary>
+    /// Gets or sets the role of the library member.
+    /// </summary>
+    public MemberRole Role { get; set; }
+}
