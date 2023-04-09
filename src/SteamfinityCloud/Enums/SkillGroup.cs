f63f87cdcaff4f36b5d91235170faac313fdea27
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-09 19:27:32 +0200

    Add SkillGroup Enum

---
+++ src/SteamfinityCloud/Enums/SkillGroup.cs
@@ -0,0 +1,24 @@
+﻿namespace Steamfinity.Cloud.Enums;
+
+public enum SkillGroup
+{
+    Unknown,
+    Silver1,
+    Silver2,
+    Silver3,
+    Silver4,
+    SilverElite,
+    SilverEliteMaster,
+    GoldNova1,
+    GoldNova2,
+    GoldNova3,
+    GoldNovaMaster,
+    MasterGuardian1,
+    MasterGuardian2,
+    MasterGuardianElite,
+    DistinguishedMasterGuardian,
+    LegendaryEagle,
+    LegendaryEagleMaster,
+    SupremeMasterFirstClass,
+    TheGlobalElite
+}
