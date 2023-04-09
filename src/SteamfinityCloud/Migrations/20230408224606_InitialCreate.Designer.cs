b380e0f9b51f575cf95c1bbfc04f53cae02416e1
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-09 16:37:10 +0200

    Re-Add InitialCreate Migration

--- src/SteamfinityCloud/Migrations/20230408224606_InitialCreate.Designer.cs
+++ src/SteamfinityCloud/Migrations/20230408224606_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Steamfinity.Cloud;
 namespace Steamfinity.Cloud.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230407151218_InitialCreate")]
+    [Migration("20230408224606_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
