d45680a55baf39bd97c9c3be0634ac2241dd4b40
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-10 16:51:23 +0200

    Allow Admins to Edit Other Users

--- src/SteamfinityCloud/Controllers/UsersController.cs
+++ src/SteamfinityCloud/Controllers/UsersController.cs
@@ -16,15 +16,29 @@ namespace Steamfinity.Cloud.Controllers;
 public sealed class UsersController : ControllerBase
 {
     private readonly UserManager<ApplicationUser> _userManager;
-    private readonly ILogger _logger;
 
-    public UsersController(UserManager<ApplicationUser> userManager, ILogger<UsersController> logger)
+    public UsersController(UserManager<ApplicationUser> userManager)
     {
         _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
-    [HttpGet("find-by-user-name")]
+    private Guid CurrentUserId
+    {
+        get
+        {
+            var nameIdentifier = User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new InvalidOperationException("The user authentication token is missing the NameIdentifier claim.");
+
+            if (!Guid.TryParse(nameIdentifier, out var userId))
+            {
+                throw new InvalidOperationException("The user authentication token NameIdentifier claim is not a valid GUID.");
+            }
+
+            return userId;
+        }
+    }
+
+    [HttpGet("search/{userName}")]
     [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -47,23 +61,26 @@ public sealed class UsersController : ControllerBase
         return Ok(result);
     }
 
-    [HttpPatch("current/user-name")]
+    [HttpPatch("{userId}/username")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> ChangeUserNameAsync(UserNameChangeRequest request)
+    public async Task<IActionResult> ChangeUserNameAsync(Guid userId, UserNameChangeRequest request)
     {
         ArgumentNullException.ThrowIfNull(request, nameof(request));
 
-        var user = await _userManager.GetUserAsync(User);
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
+        var user = await _userManager.FindByIdAsync(userId.ToString());
         if (user == null)
         {
-            _logger.LogInformation("The deleted user '{userId}' has attempted to change their username to '{newUserName}'.", userId, request.NewUserName);
             return Problem(statusCode: StatusCodes.Status401Unauthorized, detail: "user-not-found");
+        }
+
+        if (userId != CurrentUserId && !IsCurrentUserAdministrator())
+        {
+            return Problem(statusCode: StatusCodes.Status403Forbidden, detail: "access-denied");
         }
 
         var result = await _userManager.SetUserNameAsync(user, request.NewUserName);
@@ -73,40 +90,39 @@ public sealed class UsersController : ControllerBase
 
             if (errorCode == "DuplicateUserName")
             {
-                _logger.LogInformation("The user '{userId}' has attempted to change their username to '{newUserName}', but this username is already taken.", user.Id, request.NewUserName);
                 return Problem(statusCode: StatusCodes.Status409Conflict, detail: "duplicate-user-name");
             }
 
             if (errorCode == "InvalidUserName")
             {
-                _logger.LogInformation("The user '{userId}' has attempted to change their username to '{newUserName}', but this username is invalid.", user.Id, request.NewUserName);
                 return Problem(statusCode: StatusCodes.Status400BadRequest, detail: "invalid-user-name");
             }
 
-            _logger.LogError("An identity error has occurred while attempting to change the username of the user '{userId}' to '{newUserName}': '{errorCode}'.", user.Id, request.NewUserName, errorCode);
             throw new IdentityException(errorCode);
         }
 
-        _logger.LogInformation("The user '{userId}' has successfully changed their username to '{newUserName}'.", user.Id, request.NewUserName);
         return NoContent();
     }
 
-    [HttpPatch("current/password")]
+    [HttpPatch("{userId}/password")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> ChangePasswordAsync(PasswordChangeRequest request)
+    public async Task<IActionResult> ChangePasswordAsync(Guid userId, PasswordChangeRequest request)
     {
         ArgumentNullException.ThrowIfNull(request, nameof(request));
 
-        var user = await _userManager.GetUserAsync(User);
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
+        var user = await _userManager.FindByIdAsync(userId.ToString());
         if (user == null)
         {
-            _logger.LogInformation("The deleted user '{userId}' has attempted to change their password.", userId);
             return Problem(statusCode: StatusCodes.Status401Unauthorized, detail: "user-not-found");
+        }
+
+        if (userId != CurrentUserId && !IsCurrentUserAdministrator())
+        {
+            return Problem(statusCode: StatusCodes.Status403Forbidden, detail: "access-denied");
         }
 
         var result = await _userManager.ChangePasswordAsync(user, request.CurrentPassword, request.NewPassword);
@@ -116,46 +132,43 @@ public sealed class UsersController : ControllerBase
 
             if (errorCode == "PasswordMismatch")
             {
-                _logger.LogInformation("The user '{userId}' has attempted to change their password, but the provided current password is incorrect.", user.Id);
                 return Problem(statusCode: StatusCodes.Status401Unauthorized, detail: "incorrect-password");
             }
 
             if (errorCode is "PasswordTooShort" or "PasswordRequiresLower" or "PasswordRequiresUpper" or
                "PasswordRequiresDigit" or "PasswordRequiresNonAlphanumeric" or "PasswordRequiresUniqueChars")
             {
-                _logger.LogInformation("The user '{userId}' has attempted to change their password, but the provided new password is too weak.", user.Id);
                 return Problem(statusCode: StatusCodes.Status400BadRequest, detail: "password-too-weak");
             }
 
-            _logger.LogError("An identity error has occurred while attempting to change the password of the user '{userId}': '{errorCode}'.", user.Id, errorCode);
             throw new IdentityException(errorCode);
         }
 
-        _logger.LogInformation("The user '{userId}' has successfully changed their password.", user.Id);
         return NoContent();
     }
 
-    [HttpDelete("current")]
+    [HttpDelete("{userId}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> DeleteUserAsync(UserDeletionRequest request)
+    public async Task<IActionResult> DeleteUserAsync(Guid userId, UserDeletionRequest request)
     {
         ArgumentNullException.ThrowIfNull(request, nameof(request));
 
-        var user = await _userManager.GetUserAsync(User);
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
+        var user = await _userManager.FindByIdAsync(userId.ToString());
         if (user == null)
         {
-            _logger.LogInformation("The deleted user '{userId}' has attempted to delete their account again.", userId);
             return Problem(statusCode: StatusCodes.Status401Unauthorized, detail: "user-not-found");
+        }
+
+        if (userId != CurrentUserId && !IsCurrentUserAdministrator())
+        {
+            return Problem(statusCode: StatusCodes.Status403Forbidden, detail: "access-denied");
         }
 
         if (!await _userManager.CheckPasswordAsync(user, request.Password))
         {
-            _logger.LogInformation("The user '{userId}' has attempted to delete their account but provided an incorrect password.", user.Id);
             return Problem(statusCode: StatusCodes.Status401Unauthorized, detail: "incorrect-password");
         }
 
@@ -163,12 +176,14 @@ public sealed class UsersController : ControllerBase
         if (!result.Succeeded)
         {
             var errorCode = result.Errors.First().Code;
-
-            _logger.LogError("An identity error has occurred while attempting to delete the user '{userId}': '{errorCode}'.", user.Id, errorCode);
             throw new IdentityException(errorCode);
         }
 
-        _logger.LogInformation("The user '{userId}' has been successfully deleted.", user.Id);
         return NoContent();
+    }
+
+    private bool IsCurrentUserAdministrator()
+    {
+        return User.HasClaim(ClaimTypes.Role, RoleNames.Administrator);
     }
 }
