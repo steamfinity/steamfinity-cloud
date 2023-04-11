362bd409fb29fb33036bad5500ef38c3b499c445
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-04-12 00:14:42 +0200

    Rename AccountQueryBuilder Class and Improve Search

--- src/SteamfinityCloud/Extensions/AccountQueryExtension.cs
+++ src/SteamfinityCloud/Extensions/AccountQueryExtension.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 
 namespace Steamfinity.Cloud.Extensions;
 
-public static partial class AccountQueryBuilder
+public static partial class AccountQueryExtension
 {
     public static IQueryable<Account> ApplyQueryOptions(this IQueryable<Account> query, AccountQueryOptions options)
     {
@@ -43,33 +43,30 @@ public static partial class AccountQueryBuilder
 
         if (options.BannedVisibility == BannedAccountVisibility.ShowBanned)
         {
-            query = query.Where(a => a.IsCommunityBanned.HasValue && a.IsCommunityBanned.Value || a.NumberOfVACBans > 0 || a.NumberOfGameBans > 0);
+            query = query.Where(a => (a.IsCommunityBanned.HasValue && a.IsCommunityBanned.Value) || a.NumberOfVACBans > 0 || a.NumberOfGameBans > 0);
         }
         else if (options.BannedVisibility == BannedAccountVisibility.HideBanned)
         {
-            query = query.Where(a => !(a.IsCommunityBanned.HasValue && a.IsCommunityBanned.Value || a.NumberOfVACBans > 0 || a.NumberOfGameBans > 0));
+            query = query.Where(a => !((a.IsCommunityBanned.HasValue && a.IsCommunityBanned.Value) || a.NumberOfVACBans > 0 || a.NumberOfGameBans > 0));
         }
 
         if (options.Search != null)
         {
             var optimizedSearch = options.Search.OptimizeForSearch();
-            var optimizedSteamIdSearch = ulong.Parse(
-                NumberRegex().Replace(optimizedSearch
-                .Replace("steam_0:1:", string.Empty)
-                .Replace("[u:1:", string.Empty), string.Empty));
+            var numberSearch = NumberRegex().Replace(optimizedSearch.Replace("steam_0:1:", "").Replace("[u:1:", ""), "");
 
             query = query.Where(a =>
-            a.SteamId == optimizedSteamIdSearch ||
-            (a.SteamId - OtherConstants.SteamId64Base) == optimizedSteamIdSearch ||
-            ((a.SteamId - OtherConstants.SteamId64Base - 1) / 2ul) == optimizedSteamIdSearch ||
-            a.OptimizedAccountName != null && a.OptimizedAccountName.Contains(optimizedSearch) ||
-            a.OptimizedAlias != null && a.OptimizedAlias.Contains(optimizedSearch) ||
-            a.OptimizedProfileName != null && a.OptimizedProfileName.Contains(optimizedSearch) ||
-            a.OptimizedRealName != null && a.OptimizedRealName.Contains(optimizedSearch) ||
-            a.OptimizedProfileUrl != null && a.OptimizedProfileUrl.Contains(optimizedSearch) ||
-            a.OptimizedCurrentGameName != null && a.OptimizedCurrentGameName.Contains(optimizedSearch) ||
-            a.OptimizedLaunchParameters != null && a.OptimizedLaunchParameters.Contains(optimizedSearch) ||
-            a.OptimizedNotes != null && a.OptimizedNotes.Contains(optimizedSearch));
+            a.SteamId.ToString().Contains(numberSearch) ||
+            (a.SteamId - OtherConstants.SteamId64Base).ToString().Contains(numberSearch) ||
+            ((a.SteamId - OtherConstants.SteamId64Base - 1) / 2ul).ToString().Contains(numberSearch) ||
+            (a.OptimizedAccountName != null && a.OptimizedAccountName.Contains(optimizedSearch)) ||
+            (a.OptimizedAlias != null && a.OptimizedAlias.Contains(optimizedSearch)) ||
+            (a.OptimizedProfileName != null && a.OptimizedProfileName.Contains(optimizedSearch)) ||
+            (a.OptimizedRealName != null && a.OptimizedRealName.Contains(optimizedSearch)) ||
+            (a.OptimizedProfileUrl != null && a.OptimizedProfileUrl.Contains(optimizedSearch)) ||
+            (a.OptimizedCurrentGameName != null && a.OptimizedCurrentGameName.Contains(optimizedSearch)) ||
+            (a.OptimizedLaunchParameters != null && a.OptimizedLaunchParameters.Contains(optimizedSearch)) ||
+            (a.OptimizedNotes != null && a.OptimizedNotes.Contains(optimizedSearch)));
         }
 
         return query;
